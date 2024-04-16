0a6771c8c1815039b4082cfa4db16720d6227c24
DiovanaCesconetto <cesconettodiovana2@gmail.com>
2024-04-16 13:15:23 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NotificarVacinas',
+  webDir: 'www'
+};
+
+export default config;
