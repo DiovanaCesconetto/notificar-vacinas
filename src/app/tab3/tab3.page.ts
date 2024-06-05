4b0d0c7eb6d230ece4f9ec9f37c3455a06cda1e4
Renata Cavalcanti <renatapsc19@gmail.co>
2024-06-05 20:21:06 -0300

    tela

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  expanded: boolean = false;
 
   constructor() {}
 
+  toggleExpand() {
+    this.expanded = !this.expanded;
+  }
 }
